a8cb0d7b02c7cf342e8575d684bef9dbc7ff2b86
champiw <sergio.b.cast@gmail.com>
2026-08-20 19:59:45 +0200

    added helper function to search todos, delete function and health function

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,10 +10,13 @@ import (
 func main() {
 	router := gin.Default()
 
+	router.GET("/health", health)                     // get health
 	router.GET("/todos", getTodos)                    // get all todos
-	router.GET("/todos/:id", getTodoByID)             // get a single todo
-	router.POST("/todos", postTodos)                  // add a todo
+	router.GET("/todos/:id", getTodoByID)             // get single todo
+	router.POST("/todos", postTodos)                  // add todo
+	router.PATCH("/todos/:id", updateTodo)            // update todo
 	router.PATCH("/todos/:id/complete", completeTodo) // complete todo
+	router.DELETE("/todos/:id", deleteTodo)           // delete todo
 
 	router.Run("localhost:8000")
 }
@@ -28,6 +31,14 @@ var todos = []todo{
 	{ID: 1, Text: "test1", Completed: false},
 	{ID: 2, Text: "test2", Completed: true},
 	{ID: 3, Text: "test3", Completed: false},
+}
+
+// returns server health
+// TODO: update with sqlite when implemented
+func health(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+	})
 }
 
 // getTodos replies with the list of all todos as json
@@ -52,8 +63,19 @@ func postTodos(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newTodo)
 }
 
+// getTodo: Utility function that takes an ID and returns the todo and if it was found
+func getTodo(id int) (*todo, bool) {
+	for i := range todos {
+		if todos[i].ID == id {
+			return &todos[i], true
+		}
+	}
+
+	return nil, false
+}
+
 // getTodoByID locates the todo whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
+// parameter sent by the client, then returns that todo as a response.
 func getTodoByID(c *gin.Context) {
 	// Convert id to int
 	id, err := strconv.Atoi(c.Param("id"))
@@ -62,15 +84,21 @@ func getTodoByID(c *gin.Context) {
 		return
 	}
 
-	// Loop over the list of todos, looking for
-	// a todo whose ID value matches the parameter.
-	for _, a := range todos {
-		if a.ID == id {
-			c.IndentedJSON(http.StatusOK, a)
-			return
-		}
+	t, found := getTodo(id)
+
+	// todo not found
+	if !found {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+
+	// todo found
+	c.IndentedJSON(http.StatusOK, t)
+}
+
+// updateTodo updates the properties of a todo
+func updateTodo(c *gin.Context) {
+
 }
 
 func completeTodo(c *gin.Context) {
@@ -91,5 +119,31 @@ func completeTodo(c *gin.Context) {
 			return
 		}
 	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
+}
+
+// deleteTodo deletes a todo
+func deleteTodo(c *gin.Context) {
+	// Convert id to int
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid ID"})
+		return
+	}
+
+	for i := range todos {
+		if todos[i].ID == id {
+
+			// Using append function to combine two slices
+			// first slice is the slice of all the elements before the given index
+			// second slice is the slice of all the elements after the given index
+			// append function appends the second slice to the end of the first slice
+			todos = append(todos[:i], todos[i+1:]...)
+
+			c.Status(http.StatusNoContent)
+			return
+		}
+	}
+
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "todo not found"})
 }
